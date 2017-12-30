14ff6b068d7157e727643b6e708096452961df9e
rywesTech <ryan@westcott.email>
2017-12-30 13:00:03 -0800

    iOS app and arduino side are almost talking, just need to fix a few more issues.

--- UltraSky-Core/UltraSky-Core.ino
+++ UltraSky-Core/UltraSky-Core.ino
@@ -1,9 +1,132 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <WiFi.h>
+#include "SparkFunCCS811.h"
 
+#define CCS811_ADDR 0x5B //Default I2C Address
+
+char ssid[] = "Ryan's iPhone";      // your network SSID (name)
+char pass[] = "sailboat70";   // your network password
+
+int status = WL_IDLE_STATUS;
+int CO2Level, TVOCLevel;
+
+WiFiServer server(9440);
+CCS811 CO2Sensor(CCS811_ADDR);
+
+void setup() {
+  Serial.begin(9600);
+
+  CCS811Core::status returnCode = CO2Sensor.begin();
+  if (returnCode != CCS811Core::SENSOR_SUCCESS) {
+    Serial.println("CO2Sensor.begin() returned with an error.");
+    while (true); //Hang if there was a problem.
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if ( fv != "1.1.0" )
+    Serial.println("Please upgrade the firmware");
+
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 5 seconds for connection:
+    delay(5000);
+  }
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
 
+          client.println("{");
+          client.println("\"status\":\"good\",");
+          client.println("\"CO2Level\":" + String(CO2Level) + ",");
+          client.println("\"TVOCLevel\":" + String(TVOCLevel) + ",");
+          client.println("\"millis\":" + String(millis()) + ",");
+          client.println("}");
+          
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+
+  // Update levels every time we get new data:
+  if (CO2Sensor.dataAvailable()) {
+    
+    CO2Sensor.readAlgorithmResults();
+
+    CO2Level = CO2Sensor.getCO2();
+    TVOCLevel = CO2Sensor.getTVOC();
+
+    Serial.print("CO2[");
+    Serial.print(CO2Level);
+    Serial.print("] tVOC[");
+    Serial.print(TVOCLevel);
+    Serial.print("] millis[");
+    Serial.print(millis());
+    Serial.print("]");
+    Serial.println();
+  } 
 }
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
--- UltraSky-iOS/UltraSky-iOS/ViewController.swift
+++ UltraSky-iOS/UltraSky-iOS/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var IPTextField: UITextField!
     @IBOutlet weak var StatusLabel: UILabel!
     @IBOutlet weak var StartStopButton: UIButton!
+    @IBOutlet weak var ConnectButton: UIButton!
     @IBOutlet weak var DurationLabel: UILabel!
     @IBOutlet weak var DataTextView: UITextView!
     
@@ -23,10 +24,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
     var lon: Double = 0.0
     var lat: Double = 0.0
+    var CO2Level = 0
+    var TVOCLevel = 0
+    var millis = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        IPTextField.text = "172.20.1.13"
+        IPTextField.text = "172.20.10.13"
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
         
         // Ask for Authorisation from the User.
@@ -41,27 +45,68 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBAction func ConnectPressed(_ sender: Any) {
         // send a request to see if the server is up
+        let url = URL(string: "http://\(IPTextField.text!):9440")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if let data = data {
+                do {
+                    // Convert the data to JSON
+                    let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                    if let json = jsonSerialized, let status = json["status"]{
+                        print("Status: \(status)")
+                        self.CO2Level = json["CO2Level"] as! Int
+                        self.TVOCLevel = json["TVOCLevel"] as! Int
+                        self.millis = json["millis"] as! Int
+                        DispatchQueue.main.async {
+                            self.StatusLabel.text = "Status: Connected"
+                            self.ConnectButton.isEnabled = false
+                            self.IPTextField.isEnabled = false
+                        }
+                    }
+                }  catch let error as NSError {
+                    print(error.localizedDescription)
+                    DispatchQueue.main.async {
+                        self.StatusLabel.text = "Status: Failed. \(error.localizedDescription)"
+                    }
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+                DispatchQueue.main.async {
+                    self.StatusLabel.text = "Status: Failed. \(error.localizedDescription)"
+                }
+            }
+        }
+        task.resume()
     }
     
     @IBAction func StartStopPressed(_ sender: Any) {
-        // Start or stop timer
+        // Start or stop timer:
         if active {
             timer.invalidate()
+            active = false
+            StartStopButton.setTitle("Start", for: .normal)
             // output log
         } else {
             timer.fire()
+            active = true
+            StartStopButton.setTitle("Stop", for: .normal)
         }
     }
     
     @objc func update() {
+        print("CO2 Level: \(CO2Level)")
+        print("Millis: \(millis)")
         // Get GPS location
         // Get data from the sensor
         // Combine data and add to log
     }
     
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        var locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue: CLLocationCoordinate2D = manager.location!.coordinate
+        print("Got new lat, lon = \(locValue.latitude), \(locValue.longitude)")
+        lat = locValue.latitude
+        lon = locValue.longitude
+        print("Vars lat, lon = \(lat), \(lon)")
     }
     
 }
